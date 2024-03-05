ecfca97b3b7836282115ac0368f017eeb0a6fdb0
Ruan Carlos <ruancarlos.rc794@gmail.com>
2024-03-04 22:12:41 -0300

    adicionado idade

--- relembrando funcoes/main.c
+++ relembrando funcoes/main.c
@@ -4,12 +4,16 @@
 int main()
 {
     char nome[10];
+    int idade;
     printf("Ola, qual seu nome?");
     scanf("%s", nome);
-    diga_boanoite(nome);
+    printf("E qual e a sua idade?");
+    scanf("%d", &idade);
+    diga_boanoite(nome , idade);
+
     return 0;
 }
 
-void diga_boanoite(char nome[]){
-    printf(" Boa noite %s!", nome);
+void diga_boanoite(char nome[], int idade){
+    printf(" Boa noite %s!, sua idade e %d? nossa voce esta velho!", nome ,idade);
 }
